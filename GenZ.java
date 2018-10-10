9da0c64cf2060902c79a168a0f106ee4fea5d135
Christian Pennokee <cpennokee@ou.edu>
2018-10-10 14:13:56 -0500

    Convert RunTime Error to Compilation Error

--- GenZ.java
+++ GenZ.java
@@ -5,8 +5,8 @@ public class GenZ
     public static void main(String[] args)
     {
         System.out.println("One day I will learn git");
-        Comparable d = new Date();
-        System.out.println(d.compareTo(new String("String")));
+        Comparable<Date> d = new Date();
+        System.out.println(d.compareTo(new Date(2018, 1, 15)));
     }
 
 }

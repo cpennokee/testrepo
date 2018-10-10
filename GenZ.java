dabf0cbe9fda330e2a5812ceba5a98438e4fdfa0
Christian Pennokee <cpennokee@ou.edu>
2018-10-10 14:10:57 -0500

    Create Comparable object

--- GenZ.java
+++ GenZ.java
@@ -1,10 +1,12 @@
-
+import java.util.Date;
 public class GenZ
 {
 
     public static void main(String[] args)
     {
         System.out.println("One day I will learn git");
+        Comparable d = new Date();
+        System.out.println(d.compareTo(new String("String")));
     }
 
 }
